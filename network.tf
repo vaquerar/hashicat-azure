69c6af029df7ef5baf356d61f009d96808ab8d9f
Roberto Vaquerano <roberto.vaquerano@westjet.com>
2022-06-14 20:03:37 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -4,5 +4,5 @@ module "network" {
   source  = "app.terraform.io/WestJet-Vaq/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "${var.prefix}-workshop"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
