018f1259f33ddb7334dd9fe99f9f4313b9128c5f
Roberto Vaquerano <roberto.vaquerano@westjet.com>
2022-06-14 20:10:30 +0000

    Added network module

--- main.tf
+++ main.tf
@@ -110,6 +110,7 @@ resource "azurerm_virtual_machine" "catapp" {
 
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
+  
 
   storage_image_reference {
     publisher = var.image_publisher
@@ -135,7 +136,8 @@ resource "azurerm_virtual_machine" "catapp" {
     disable_password_authentication = false
   }
 
-  tags = {}
+  tags = {Department = "devops"
+  Billable = "true"}
 
   # Added to allow destroy to work correctly.
   depends_on = [azurerm_network_interface_security_group_association.catapp-nic-sg-ass]
